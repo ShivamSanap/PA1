9f9311ce2b880129db4969df42412403770a062f
ShivamSanap <shivamsanap08@gmail.com>
2025-08-18 14:23:45 +0530

    added nn

--- neural_net/src/matrix_operation.c
+++ neural_net/src/matrix_operation.c
@@ -38,7 +38,14 @@ Matrix MatrixOperation::ReorderedMatMul(const Matrix& A, const Matrix& B) {
 	Matrix C(n,m);
 	
 //----------------------------------------------------- Write your code here ----------------------------------------------------------------
-    
+    for (size_t i = 0; i < n; ++i) {
+		for (size_t l = 0; l < k; ++l) {
+			element_t a_il = A(i, l);
+			for (size_t j = 0; j < m; ++j) {
+				C(i, j) += a_il * B(l, j);
+			}
+		}
+	}
 
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -60,7 +67,22 @@ Matrix MatrixOperation::UnrolledMatMul(const Matrix& A, const Matrix& B) {
 
     const int UNROLL = 4;
 //----------------------------------------------------- Write your code here ----------------------------------------------------------------
-    
+    for (size_t i = 0; i < n; ++i) {
+		for (size_t l = 0; l < k; ++l) {
+			element_t a_il = A(i, l);
+			size_t j = 0;
+			for (; j + UNROLL - 1 < m; j += UNROLL) {
+				C(i, j)     += a_il * B(l, j);
+				C(i, j + 1) += a_il * B(l, j + 1);
+				C(i, j + 2) += a_il * B(l, j + 2);
+				C(i, j + 3) += a_il * B(l, j + 3);
+			}
+			// tail: handles m not being a multiple of UNROLL
+			for (; j < m; ++j) {
+				C(i, j) += a_il * B(l, j);
+			}
+		}
+	}
 
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -83,7 +105,24 @@ Matrix MatrixOperation::TiledMatMul(const Matrix& A, const Matrix& B) {
 	int k_max = 0;
 	int j_max = 0;
 //----------------------------------------------------- Write your code here ----------------------------------------------------------------
-    
+    for (size_t ii = 0; ii < n; ii += T) {
+		i_max = std::min(ii + T, n);
+		for (size_t kk = 0; kk < k; kk += T) {
+			k_max = std::min(kk + T, k);
+			for (size_t jj = 0; jj < m; jj += T) {
+				j_max = std::min(jj + T, m);
+ 
+				for (size_t i = ii; i < (size_t)i_max; ++i) {
+					for (size_t l = kk; l < (size_t)k_max; ++l) {
+						element_t a_il = A(i, l);
+						for (size_t j = jj; j < (size_t)j_max; ++j) {
+							C(i, j) += a_il * B(l, j);
+						}
+					}
+				}
+			}
+		}
+	}
 
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -103,7 +142,23 @@ Matrix MatrixOperation::VectorizedMatMul(const Matrix& A, const Matrix& B) {
     Matrix C(n, m);
 //----------------------------------------------------- Write your code here ----------------------------------------------------------------
     
-
+for (size_t i = 0; i < n; ++i) {
+		for (size_t l = 0; l < k; ++l) {
+			__m256d a_il = _mm256_set1_pd(A(i, l));
+			size_t j = 0;
+			for (; j + 3 < m; j += 4) {
+				__m256d b_vec = _mm256_loadu_pd(&B(l, j));
+				__m256d c_vec = _mm256_loadu_pd(&C(i, j));
+				c_vec = _mm256_add_pd(c_vec, _mm256_mul_pd(a_il, b_vec));
+				_mm256_storeu_pd(&C(i, j), c_vec);
+			}
+			// tail: handles m not being a multiple of 4
+			double a_il_s = A(i, l);
+			for (; j < m; ++j) {
+				C(i, j) += a_il_s * B(l, j);
+			}
+		}
+	}
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
     return C;
@@ -125,7 +180,18 @@ Matrix MatrixOperation::Transpose(const Matrix& A) {
 	// This is a simple implementation, more advanced techniques can be applied
 	// Write your code here and commnent the above code
 //----------------------------------------------------- Write your code here ----------------------------------------------------------------
-    
+    const size_t T = 32;
+	for (size_t ii = 0; ii < rows; ii += T) {
+		size_t i_max = std::min(ii + T, rows);
+		for (size_t jj = 0; jj < cols; jj += T) {
+			size_t j_max = std::min(jj + T, cols);
+			for (size_t i = ii; i < i_max; ++i) {
+				for (size_t j = jj; j < j_max; ++j) {
+					result(j, i) = A(i, j);
+				}
+			}
+		}
+	}
 
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
